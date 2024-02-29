3081876d9241e3611042bea45f0cefa5c090e149
EngrGord <chijokeogbuanya@gmail.com>
2024-02-29 13:59:00 +0100

    Hide overflow in pages.

--- packages/nextjs/app/pools/page.tsx
+++ packages/nextjs/app/pools/page.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 
 const page: NextPage = () => {
   return (
-    <div className="max-w-full">
+    <div className="max-w-full overflow-hidden">
       <Pools />
     </div>
   );
--- packages/nextjs/app/profiles/page.tsx
+++ packages/nextjs/app/profiles/page.tsx
@@ -6,7 +6,7 @@ import { NextPage } from "next";
 
 const Page: NextPage = () => {
   return (
-    <div className="max-w-full">
+    <div className="max-w-full overflow-hidden">
       <ProfilesList />
     </div>
   );
