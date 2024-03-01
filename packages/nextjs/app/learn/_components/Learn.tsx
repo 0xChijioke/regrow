9a26c77a2cd170dca4997bcbbdb0c9537ee3c926
EngrGord <chijokeogbuanya@gmail.com>
2024-03-01 10:28:22 +0100

    Added learn page.

---
+++ packages/nextjs/app/learn/_components/Learn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Learn = () => {
+  return <div className="w-full flex justify-center">Learn how to use regrow app</div>;
+};
+
+export default Learn;
---
+++ packages/nextjs/app/learn/page.tsx
@@ -0,0 +1,12 @@
+import Learn from "./_components/Learn";
+import { NextPage } from "next";
+
+const page: NextPage = () => {
+  return (
+    <div>
+      <Learn />
+    </div>
+  );
+};
+
+export default page;
