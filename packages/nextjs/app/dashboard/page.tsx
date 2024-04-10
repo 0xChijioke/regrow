1d9bf95604a6029090609c48cabee9162e21e48d
EngrGord <chijokeogbuanya@gmail.com>
2024-04-10 23:39:53 +0100

    dashboard

---
+++ packages/nextjs/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "./_components/Dashboard";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon, ShoppingCartIcon, UsersIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, HomeIcon, ShoppingCartIcon, UsersIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -35,9 +35,9 @@ export const menuLinks: HeaderMenuLink[] = [
     icon: <UsersIcon className="h-4 w-4" />,
   },
   {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: <HomeIcon className="h-4 w-4" />,
   },
 ];
 
